1b50830fd2532e5a04a71cde1062ddb8a2f2b0a0
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2020-04-03 21:01:23 +0200

    Add demo app layout

--- demo/src/main/java/otpview/demo/MainActivity.kt
+++ demo/src/main/java/otpview/demo/MainActivity.kt
@@ -2,10 +2,12 @@ package otpview.demo
 
 import android.app.Activity
 import android.os.Bundle
+import otpview.OtpView
 
 internal class MainActivity : Activity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    val otpView = findViewById<OtpView>(R.id.otp)
   }
 }
