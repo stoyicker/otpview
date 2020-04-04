eb7a79e50308c9e3db1fa0ce46c53da2284048f2
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2020-04-04 18:40:54 +0200

    Add functionality to Github and Linkedin buttons

--- demo/src/main/java/otpview/demo/MainActivity.kt
+++ demo/src/main/java/otpview/demo/MainActivity.kt
@@ -1,13 +1,22 @@
 package otpview.demo
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import otpview.OtpView
+import android.view.View
 
 internal class MainActivity : Activity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    val otpView = findViewById<OtpView>(R.id.otp)
   }
+
+  fun onLinkedin(ignored: View) =
+      startActivity(Intent(Intent.ACTION_VIEW,
+          Uri.parse("http://www.linkedin.com/profile/view?id=jorgediazbenitosoriano")))
+
+  fun onGithub(ignored: View) =
+      startActivity(Intent(Intent.ACTION_VIEW,
+          Uri.parse("http://www.github.com/stoyicker/otpview")))
 }
