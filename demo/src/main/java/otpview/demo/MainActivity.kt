c3ca4bdfb44c30dee91f6ea6b3ed24a49c77628f
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2020-04-05 19:53:56 +0200

    Add blank demo app

---
+++ demo/src/main/java/otpview/demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package otpview.demo
+
+import android.app.Activity
+import android.os.Bundle
+
+internal class MainActivity : Activity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
