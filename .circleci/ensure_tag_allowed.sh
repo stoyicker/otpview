691669d42c3164e2e74cf698927e78dbac84840e
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2020-04-10 18:07:34 +0200

    Add tagging permission check script

---
+++ .circleci/ensure_tag_allowed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+if [[ "$CIRCLE_PROJECT_USERNAME" = "$CIRCLE_USERNAME" ]]; then
+   exit 0
+else
+   echo "User $CIRCLE_USERNAME is not $CIRCLE_PROJECT_USERNAME; not allowed to trigger the release workflow!"
+   exit 1
+fi
