19b6443e0f4f3b3be81e69b5ca47e3303833c148
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2020-04-10 20:07:32 +0200

    Add release creation script

---
+++ .circleci/new_release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+usage() {
+  echo "Usage: $0 -v <version> [-r <revision=HEAD>]" >&2
+  exit 1
+}
+
+VERSION=""
+REVISION="HEAD"
+
+while getopts ":v:r" opt; do
+  case $opt in
+  v)
+    VERSION=$OPTARG
+    ;;
+  r)
+    REVISION=$OPTARG
+    ;;
+  \?)
+    echo "Invalid option: -$OPTARG" >&2
+    exit 1
+    ;;
+  *)
+    usage
+    ;;
+  esac
+done
+
+if [ -z "$VERSION" ]; then
+  usage
+fi
+
+git tag ${VERSION} ${REVISION} -m "${VERSION}" && git push --tags
